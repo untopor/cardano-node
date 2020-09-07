f038e2e7f20f67b8d80d13d0873df50bc8bcecb9
John Ky <john.ky@iohk.io>
2020-09-08 05:47:36 +1000

    Byron-Shelley chairman tests (POSIX only)

--- cardano-node-chairman/test/Main.hs
+++ cardano-node-chairman/test/Main.hs
@@ -5,13 +5,16 @@ module Main
 import           Hedgehog.Main (defaultMain)
 import           System.IO (IO)
 
-import qualified Test.Cardano.Node.Chairman.Byron
-import qualified Test.Cardano.Node.Chairman.Shelley
-import qualified Test.Common.NetworkSpec
+-- import qualified Test.Cardano.Node.Chairman.Byron
+import qualified Test.Cardano.Node.Chairman.ByronShelley
+-- import qualified Test.Cardano.Node.Chairman.Shelley
+-- import qualified Test.Common.NetworkSpec
 
 main :: IO ()
 main = defaultMain
-  [ Test.Cardano.Node.Chairman.Byron.tests
-  , Test.Cardano.Node.Chairman.Shelley.tests
-  , Test.Common.NetworkSpec.tests
+  [ -- Test.Cardano.Node.Chairman.Byron.tests
+  -- ,
+  Test.Cardano.Node.Chairman.ByronShelley.tests
+  -- , Test.Cardano.Node.Chairman.Shelley.tests
+  -- , Test.Common.NetworkSpec.tests
   ]
